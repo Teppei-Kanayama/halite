fe2934bf3223dbadf5a335e6effcd184f928e0cf
Teppei Kanayama <kanayama.teppei@gmail.com>
2020-06-19 23:38:20 +0900

    good

--- main.py
+++ main.py
@@ -18,39 +18,46 @@ direction_mapper = dict(north=ShipAction.NORTH, east=ShipAction.EAST, south=Ship
 
 
 class ActionManager:
-    def __init__(self, ship, board):
+    def __init__(self, ship, board, me, size):
         self._ship = ship
         self._board = board
+        self._me = me
+        self._size = size
 
     def get_action_options(self):
-        ship_positions = [s.position for s in self._board.ships.values()]  # [(5, 15), (15, 15), (5, 5), (15, 5)]
-        shipyard_positions = [s.position for s in self._board.shipyards.values()]
-        my_position = self._ship.position  # (5, 15)
-        dangerous_positions = self._get_dangerous_positions(ship_positions, shipyard_positions, my_position)
-        safe_directions = self._get_safe_directions(dangerous_positions, my_position)
+        ship_positions = [s.position for s in self._board.ships.values()]
+        shipyard_positions = [s.position for k, s in self._board.shipyards.items() if not f'{self._me.id}-' in k]
+        my_position = self._ship.position
+        dangerous_positions = self._get_dangerous_positions(ship_positions, shipyard_positions, my_position, self._size)
+        safe_directions = self._get_safe_directions(dangerous_positions, my_position, self._size)
         return safe_directions
 
     @staticmethod
-    def _get_dangerous_positions(ship_positions, shipyard_positions, my_position):
+    def _get_dangerous_positions(ship_positions, shipyard_positions, my_position, size=21):
         dangerous_positions = []
+
+        # ship
         ship_positions = copy.copy(ship_positions)
         ship_positions.remove(my_position)
         directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
         for pos in ship_positions:
             dangerous_positions.append(pos)
             for dir in directions:
-                dangerous_positions.append(((pos[0] + dir[0]) % 21, (pos[1] + dir[1]) % 21))
+                dangerous_positions.append(((pos[0] + dir[0]) % size, (pos[1] + dir[1]) % size))
 
-        # TODO: shipyard
+        # shipyard
+        for pos in shipyard_positions:
+            dangerous_positions.append(pos)
+
         return dangerous_positions
 
     @staticmethod
-    def _get_safe_directions(dangerous_positions, my_position):
+    def _get_safe_directions(dangerous_positions, my_position, size=21):
         safe_directions = []
-        directions = dict(north=(0, -1), south=(0, 1), east=(1, 0), west=(-1, 0), stay=(0, 0))
+        directions = dict(east=(1, 0), west=(-1, 0), north=(0, 1), south=(0, -1), stay=(0, 0))
         for direction, diff in directions.items():
-            target_x = (my_position[0] + diff[0]) % 21
-            target_y = (my_position[1] + diff[1]) % 21
+            target_x = (my_position[0] + diff[0]) % size
+            target_y = (my_position[1] + diff[1]) % size
             if (target_x, target_y) not in dangerous_positions:
                 safe_directions.append(direction)
         return safe_directions
@@ -70,34 +77,13 @@ def agent(obs, config):
 
     # STEP2: 動いていい場所を消める
     for ship in me.ships:
-        action_manager = ActionManager(ship, board)
+        action_manager = ActionManager(ship, board, me, size)
         safe_directions = action_manager.get_action_options()
 
     # STEP3: 目的地を決める
-        if ship.next_action is None:
-
-            # Part 1: Set the ship's state
-            if ship.halite <= 500:  # If cargo is too low, collect halite
-                ship_states[ship.id] = "COLLECT"
-            else:  # If cargo gets very big, deposit halite
-                ship_states[ship.id] = "DEPOSIT"
-
-            # Part 2: Use the ship's state to select an action
-            if ship_states[ship.id] == "COLLECT":
-                # If halite at current location running low,
-                # move to the adjacent square containing the most halite
-                if ship.cell.halite < 100:
-                    neighbors = [ship.cell.north.halite, ship.cell.east.halite,
-                                 ship.cell.south.halite, ship.cell.west.halite]
-                    random.shuffle(neighbors)
-                    if len(neighbors):
-                        best = max(range(len(neighbors)), key=neighbors.__getitem__)
-                        ship.next_action = directions[best]
-            elif ship_states[ship.id] == "DEPOSIT":
-                # Move towards shipyard to deposit cargo
-                direction = getDirTo(ship.position, me.shipyards[0].position, size)
-                if direction: ship.next_action = direction
-            else:
-                pass  # no chance
+        direction = np.random.choice(safe_directions)
+        # direction = safe_directions[0]
+        if direction != 'stay':
+            ship.next_action = direction_mapper[direction]
 
     return me.next_actions
