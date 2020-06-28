1edd28c2883a82e1d63951e0941ee5054e6470cc
Teppei Kanayama <kanayama.teppei@gmail.com>
2020-06-28 18:38:42 +0900

    search logig

--- halite/ship/decide_ship_actions.py
+++ halite/ship/decide_ship_actions.py
@@ -35,8 +35,27 @@ def decide_one_ship_action(ship, me, board, size, already_convert):
                 return None, already_convert
             return direction_mapper[direction], already_convert
 
+    # 探索範囲内で閾値以上のhaliteがある地点のうち、最も近い地点に移動する
+    threshold = np.percentile([cell.halite for cell in board.cells.values()], 80)
+    search_range = [((ship.position[0] + i) % size, (ship.position[0] + j) % size) for i in range(5) for j in range(5)]
+    target_map = {pos: board.cells[pos].halite for pos in search_range if board.cells[pos].halite >= threshold}
+
+    def calculate_distance(target):
+        distance_x = min((target[0] - ship.position[0]) % size, (ship.position[0] - target[0]) % size)
+        distance_y = min((target[1] - ship.position[1]) % size, (ship.position[1] - target[1]) % size)
+        return distance_x + distance_y
+
+    if target_map:
+        destination = min(target_map.keys(), key=calculate_distance)
+        direction_scores = get_direction_to_destination(ship.position, destination, size=size)
+        safe_direction_scores = {k: v for k, v in direction_scores.items() if k in safe_directions}
+        if len(safe_direction_scores) > 0:
+            direction = max(safe_direction_scores, key=safe_direction_scores.get)
+            if direction == 'stay':
+                return None, already_convert
+            return direction_mapper[direction], already_convert
+
     # その他ならランダムに移動する
-    # TODO: 探索の仕方を工夫する
     direction = np.random.choice(safe_directions)
     if direction == 'stay':
         return None, already_convert
