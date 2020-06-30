e084847b336ac4163972449fd846ad5f1bc8e65e
Teppei Kanayama <kanayama.teppei@gmail.com>
2020-06-30 19:36:58 +0900

    refactor

--- halite/ship/decide_ship_actions.py
+++ halite/ship/decide_ship_actions.py
@@ -5,23 +5,31 @@ from halite.ship.strategy import decide_direction_for_rich_position, decide_dire
 from halite.utils.constants import direction_mapper, action_to_direction, direction_vector
 
 
+MAXIMUM_NUM_OF_SHIPYARDS = 2
+MINE_HALITE_WHEN_HALITE_UNDER_GROUND_IS_OVER = 100
+GO_SHIPYARD_WHEN_CARGO_IS_OVER = 500
+
+
 # TODO: shipごとにいくつかのstrategyを混ぜていきたい
-def decide_one_ship_action(ship, me, board, size, safe_directions: List[Tuple[int, int]], already_convert: bool):
+def decide_one_ship_action(ship, me, board, size: int, safe_directions: List[Tuple[int, int]], already_convert: bool):
     # 動ける場所がないなら動かない
+    # TODO: ランダムに動いた方がいいかもしれない
     if len(safe_directions) == 0:
         return None
 
     # 下記の条件を満たす場合shipyardにconvertする
     # shipyardsが少ない・haliteが十分にある・stayが安全である・まだこのターンにconvertしていない
-    if len(me.shipyards) <= min((board.step // 80), 1) and me.halite >= 500 and 'stay' in safe_directions and not already_convert:
+    # TODO: 一番halieが多いやつがconvertしたほうがお得
+    # TODO: オリジナルの関数にしたがう
+    if len(me.shipyards) < min((board.step // 80), MAXIMUM_NUM_OF_SHIPYARDS) and me.halite >= 500 and 'stay' in safe_directions and not already_convert:
         return ShipAction.CONVERT
 
     # その場にhaliteがたくさんあるなら拾う
-    if ship.cell.halite > 100 and 'stay' in safe_directions:
+    if ship.cell.halite > MINE_HALITE_WHEN_HALITE_UNDER_GROUND_IS_OVER and 'stay' in safe_directions:
         return None
 
     # haliteをたくさん載せているならshipyardsに帰る
-    if ship.halite > 500 and len(me.shipyards) > 0:
+    if ship.halite > GO_SHIPYARD_WHEN_CARGO_IS_OVER and len(me.shipyards) > 0:
         direction = decide_direction_for_shipyard(me, ship, safe_directions, size)
         return direction_mapper[direction]
 
@@ -60,7 +68,8 @@ def decide_ship_actions(me, board, size):
     return actions, fixed_positions
 
 
-def add_fixed_position(fixed_positions, action, my_position, size) -> None:
+# すでに行動が確定したshipの位置を保存する。
+def add_fixed_position(fixed_positions: List[Tuple[int, int]], action: ShipAction, my_position: Tuple[int, int], size: int) -> None:
     if action == ShipAction.CONVERT:
         return
     if action is None:
