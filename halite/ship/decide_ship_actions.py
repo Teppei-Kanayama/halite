88eafe74836c32c8083a63cb7a9bc2f42c4b46e9
Teppei Kanayama <kanayama.teppei@gmail.com>
2020-07-01 00:02:17 +0900

    atacker and collector

--- halite/ship/decide_ship_actions.py
+++ halite/ship/decide_ship_actions.py
@@ -6,22 +6,22 @@ from halite.ship.strategy import decide_direction_for_shipyard, decide_direction
 from halite.utils.constants import direction_mapper, action_to_direction, direction_vector
 
 
-MAXIMUM_NUM_OF_SHIPYARDS = 2
-MINE_HALITE_WHEN_HALITE_UNDER_GROUND_IS_OVER = 100
-GO_SHIPYARD_WHEN_CARGO_IS_OVER = 500
-
 ship_roles = {}
 next_is_collector = 1
 
 
 def decide_collector_action(ship, me, board, size: int, safe_directions: List[Tuple[int, int]], already_convert: bool,
                             responsive_area: List[Tuple[int, int]], enemy_ship_positions):
+    MAXIMUM_NUM_OF_SHIPYARDS = 1
+    MINE_HALITE_WHEN_HALITE_UNDER_GROUND_IS_OVER = 100
+
     # 「最終ターン」かつ「haliteを500以上積んでいる」ならばconvertする
     if board.step == 398 and ship.halite > 500:
         return ShipAction.CONVERT
 
-    # 動ける場所がないなら動かない
-    # TODO: ランダムに動いた方がいいかもしれない
+    # 動ける場所がないならconvertする
+    if len(safe_directions) == 0 and ship.halite > 500:
+        return ShipAction.CONVERT
     if len(safe_directions) == 0:
         return None
 
@@ -36,22 +36,31 @@ def decide_collector_action(ship, me, board, size: int, safe_directions: List[Tu
     if ship.cell.halite > MINE_HALITE_WHEN_HALITE_UNDER_GROUND_IS_OVER and 'stay' in safe_directions:
         return None
 
-    # 「序盤ではない」かつ「haliteをたくさん載せている」ならshipyardsに帰る
-    if ship.halite > GO_SHIPYARD_WHEN_CARGO_IS_OVER and len(me.shipyards) > 0 and board.step > 80:
-        direction = decide_direction_for_shipyard(me, ship, safe_directions, size)
-        return direction_mapper[direction]
-
     # 閾値以上のhaliteがある場所を探す
     direction = decide_direction_in_responsive_area(board, ship, size, safe_directions, responsive_area, halite_threshold=100)
     if direction:
         return direction_mapper[direction]
 
-    direction = attack_heavy_nearest_ship(ship, size, safe_directions, enemy_ship_positions)
-    return direction_mapper[direction]
+    # 閾値以上のhaliteがある場所を探す
+    direction = decide_direction_in_responsive_area(board, ship, size, safe_directions, responsive_area, halite_threshold=50)
+    if direction:
+        return direction_mapper[direction]
+
+    # 閾値以上のhaliteがある場所を探す
+    direction = decide_direction_in_responsive_area(board, ship, size, safe_directions, responsive_area, halite_threshold=250)
+    if direction:
+        return direction_mapper[direction]
+
+    # 閾値以上のhaliteがある場所を探す
+    direction = decide_direction_in_responsive_area(board, ship, size, safe_directions, responsive_area, halite_threshold=0)
+    if direction:
+        return direction_mapper[direction]
 
 
 def decide_attacker_action(ship, me, board, size: int, safe_directions: List[Tuple[int, int]], already_convert: bool,
                             responsive_area: List[Tuple[int, int]], enemy_ship_positions):
+    GO_SHIPYARD_WHEN_CARGO_IS_OVER = 500
+
     # 「最終ターン」かつ「haliteを500以上積んでいる」ならばconvertする
     if board.step == 398 and ship.halite > 500:
         return ShipAction.CONVERT
@@ -61,25 +70,9 @@ def decide_attacker_action(ship, me, board, size: int, safe_directions: List[Tup
     if len(safe_directions) == 0:
         return None
 
-    # 下記の条件を満たす場合shipyardにconvertする
-    # shipyardsが少ない・haliteが十分にある・stayが安全である・まだこのターンにconvertしていない
-    # TODO: 一番halieが多いやつがconvertしたほうがお得
-    # TODO: オリジナルの関数にしたがうようにする
-    if len(me.shipyards) < min((board.step // 80 + 1), MAXIMUM_NUM_OF_SHIPYARDS) and me.halite >= 500 and 'stay' in safe_directions and not already_convert:
-        return ShipAction.CONVERT
-
-    # その場にhaliteがたくさんあるなら拾う
-    if ship.cell.halite > MINE_HALITE_WHEN_HALITE_UNDER_GROUND_IS_OVER and 'stay' in safe_directions:
-        return None
-
-    # 「序盤ではない」かつ「haliteをたくさん載せている」ならshipyardsに帰る
-    if ship.halite > GO_SHIPYARD_WHEN_CARGO_IS_OVER and len(me.shipyards) > 0 and board.step > 80:
+    # 「haliteをたくさん載せている」ならshipyardsに帰る
+    if ship.halite > GO_SHIPYARD_WHEN_CARGO_IS_OVER and len(me.shipyards) > 0:
         direction = decide_direction_for_shipyard(me, ship, safe_directions, size)
-        return direction_mapper[direction]
-
-    # 閾値以上のhaliteがある場所を探す
-    direction = decide_direction_in_responsive_area(board, ship, size, safe_directions, responsive_area, halite_threshold=100)
-    if direction:
         return direction_mapper[direction]
 
     direction = attack_heavy_nearest_ship(ship, size, safe_directions, enemy_ship_positions)
@@ -95,7 +88,6 @@ def decide_ship_actions(me, board, size):
     already_convert = False
     responsive_areas = get_nearest_areas(me.ships, size)
 
-    print(ship_roles)
     for ship in me.ships:
         if ship.id not in ship_roles.keys():
             ship_roles[ship.id] = 'collector' if next_is_collector else 'attacker'
@@ -119,7 +111,13 @@ def decide_ship_actions(me, board, size):
                                        fixed_positions=fixed_positions)
         safe_directions = action_manager.get_action_options()
         responsive_area = responsive_areas[ship.id]
-        action = decide_collector_action(ship, me, board, size, safe_directions, already_convert, responsive_area, enemy_ship_positions)
+        if ship_roles[ship.id] == 'collector':
+            action = decide_collector_action(ship, me, board, size, safe_directions, already_convert, responsive_area, enemy_ship_positions)
+        elif ship_roles[ship.id] == 'attacker':
+            action = decide_attacker_action(ship, me, board, size, safe_directions, already_convert, responsive_area, enemy_ship_positions)
+        else:
+            raise NotImplementedError
+
         add_fixed_position(fixed_positions, action, my_position, size)
         if action:
             actions[ship.id] = action
