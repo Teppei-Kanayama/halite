bd87895597b052c4ff77ad21f365fbdbd9cd4db3
Teppei Kanayama <kanayama.teppei@gmail.com>
2020-07-04 20:25:05 +0900

    動く

--- halite/ship/roll/attacker.py
+++ halite/ship/roll/attacker.py
@@ -5,14 +5,12 @@ from halite.ship.roll.roll_template import RollTemplate
 from halite.ship.strategy import decide_direction_for_shipyard, attack_target_shipyard
 from halite.ship.strategy import attack_heavy_target_ship, attack_heavy_target_ship2
 from halite.utils.constants import direction_mapper
+from halite.utils.strategy_constants import *
 
 
 class Attacker(RollTemplate):
 
     def decide_next_action(self):
-        GO_SHIPYARD_WHEN_CARGO_IS_OVER = 300
-        ATTACK_SHIPYARD_IS_LESS = 100
-
         # 「最終ターン」かつ「haliteを500以上積んでいる」ならばconvertする
         if self._step == 398 and self._my_halite > 500:
             return ShipAction.CONVERT, 'final_convert'
@@ -24,12 +22,12 @@ class Attacker(RollTemplate):
             return np.random.choice(list(direction_mapper.values())), 'random_walk'
 
         # 「haliteをたくさん載せている」ならshipyardsに帰る
-        if self._my_halite > GO_SHIPYARD_WHEN_CARGO_IS_OVER and len(self._ally_shipyard_ids) > 0:
+        if self._my_halite > ATTACKER_GO_SHIPYARD_WHEN_CARGO_IS_OVER and len(self._ally_shipyard_ids) > 0:
             direction = decide_direction_for_shipyard(ally_shipyard_ids=self._ally_shipyard_ids, my_position=self._my_position,
                                                       safe_directions=self._safe_directions, size=self._size)
             return direction_mapper[direction], 'go_home'
 
-        if self._my_halite < ATTACK_SHIPYARD_IS_LESS and self._step >= 350:
+        if self._my_halite < ATTACKER_ATTACK_SHIPYARD_IS_LESS and self._step >= ATTACKER_START_SHIPYARD_ATTACK:
             direction = attack_target_shipyard(target_enemy_id=self._target_enemy_id, my_position=self._my_position, size=self._size,
                                                safe_directions_without_shipyards=self._safe_directions_without_shipyards,
                                                enemy_shipyard_ids=self._enemy_shipyard_ids)
